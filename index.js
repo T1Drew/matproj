09d048466162b8a7ed63e2413bb04efe24192f14
Andrew Parker <parkerandrew855@gmail.com>
2020-06-22 09:54:44 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+});
+
+$(document).ready(function(){
+    $('.carousel').carousel(); //carousel init
+
+    $('.carousel-slider').carousel({full_width: true,
+                                   indicators: true});
+});
